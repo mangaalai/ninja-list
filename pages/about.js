40221e326919a2054b2611992ba5c05dd4c01300
mangaalai <munkhtulgasss@gmail.com>
2021-06-04 14:19:30 +0800

    initial

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+
+const About = () => {
+    return (
+        <>
+            <Head>
+            <title>Ninja List | About</title>
+            <meta name="keywords" content="ninjas" />
+                
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Consequat ullamco id ea adipisicing irure do anim culpa consequat velit reprehenderit minim. Enim nostrud officia tempor non laborum irure commodo sunt ea eu sunt mollit id in. Sunt aute id occaecat tempor nostrud anim et. Irure veniam labore ea reprehenderit dolore nulla nostrud excepteur.</p>
+                <p>Consequat ullamco id ea adipisicing irure do anim culpa consequat velit reprehenderit minim. Enim nostrud officia tempor non laborum irure commodo sunt ea eu sunt mollit id in. Sunt aute id occaecat tempor nostrud anim et. Irure veniam labore ea reprehenderit dolore nulla nostrud excepteur.</p>
+            </div>
+        </>  
+    );
+}
+ 
+export default About;
---
+++ pages/ninjas/test.js
@@ -0,0 +1,11 @@
+const Test = () => {
+    return ( 
+        <div>
+            <h1>Test page</h1>
+            <p>Proident labore pariatur aliqua cillum id. Proident laboris veniam nisi eiusmod. Ea exercitation deserunt dolor quis eiusmod do minim Lorem mollit duis amet enim. Nostrud qui esse anim non nisi eu minim nulla est ex deserunt veniam ad. Culpa laborum Lorem cupidatat duis consectetur commodo officia esse labore irure ad. Nostrud tempor esse irure reprehenderit ea duis aliquip laboris amet.</p>
+            <p>Proident labore pariatur aliqua cillum id. Proident laboris veniam nisi eiusmod. Ea exercitation deserunt dolor quis eiusmod do minim Lorem mollit duis amet enim. Nostrud qui esse anim non nisi eu minim nulla est ex deserunt veniam ad. Culpa laborum Lorem cupidatat duis consectetur commodo officia esse labore irure ad. Nostrud tempor esse irure reprehenderit ea duis aliquip laboris amet.</p>
+        </div>
+     );
+}
+ 
+export default Test;
